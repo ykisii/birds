95af47ec43afdff16e8b2327bc0d5da0fa8a56de
ykisii <ishiiyuuki@gmail.com>
2023-04-21 23:28:10 +0900

    modify: container variable

--- bird.js
+++ bird.js
@@ -1,23 +1,24 @@
 export class Bird {
   #app;
+  #container;
   constructor(app) {
     this.#app = app;
   }
 
   appear(x, y) {
-    const container = new PIXI.Container();
-    this.#app.stage.addChild(container);
+    this.#container = new PIXI.Container();
+    this.#app.stage.addChild(this.#container);
     const texure = PIXI.Texture.from('smallbird_1.png');
     const bird = new PIXI.Sprite(texure);
     bird.anchor.set(0.5)
     bird.scale.set(0.5);
-    container.addChild(bird);
-    container.x = x;
-    container.y = y;
+    this.#container.addChild(bird);
+    this.#container.x = x;
+    this.#container.y = y;
     this.#app.ticker.add((delta) => {
-      if (container.y < this.#app.screen.height / 2) {
-        container.x += 1;
-        container.y += 1;
+      if (this.#container.y < this.#app.screen.height / 2) {
+        this.#container.x += 1;
+        this.#container.y += 1;
       }
     })
   }

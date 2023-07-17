a2a9a9aac7bc9d5ed9790edaa9df98feeeeee2d7
ykisii <ishiiyuuki@gmail.com>
2023-07-17 22:41:24 +0900

    add: start button image

--- title.js
+++ title.js
@@ -2,18 +2,26 @@ export class Title {
   #container;
   #app;
   #img;
+  #img2;
 
   constructor(app) {
     this.#app = app;
     this.#container = new PIXI.Container();
     this.#container.visible = false;
     this.#app.stage.addChild(this.#container);
-    const texure = PIXI.Texture.from('birds-title.png');
-    this.#img = new PIXI.Sprite(texure);
+    const title = PIXI.Texture.from('birds-title.png');
+    this.#img = new PIXI.Sprite(title);
     this.#img.anchor.set(0);
     this.#img.interactive = true;
-    this.#img.buttonMode = true;
+    this.#img.buttonMode = false;
     this.#container.addChild(this.#img); 
+
+    const start = PIXI.Texture.from('birds-start.png');
+    this.#img2 = new PIXI.Sprite(start);
+    this.#img2.anchor.set(40);
+    this.#img2.interactive = true;
+    this.#img2.buttonMode = true;
+    this.#container.addChild(this.#img2); 
   }
 
   visible(visible) {

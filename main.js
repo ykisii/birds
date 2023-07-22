82d6cecb9d76a3f81b765d265d79bd9e4b84d061
ykisii <ishiiyuuki@gmail.com>
2023-07-22 23:55:02 +0900

    mod: centering

--- main.js
+++ main.js
@@ -11,8 +11,11 @@ let app = new PIXI.Application({
 // let el = document.getElementById("app");
 // el.appendChild(app.view);
 document.getElementById("app").appendChild(app.view);
-let sprite = PIXI.Sprite.from('birds-title.png');
-app.stage.addChild(sprite);
+let title = PIXI.Sprite.from('birds-title.png');
+title.anchor.set(0.5);
+title.position.x = app.view.width / 2;
+title.position.y = app.view.height / 2 - 20;
+app.stage.addChild(title);
 /*
 const title = new Title(app);
 title.visible(true);
